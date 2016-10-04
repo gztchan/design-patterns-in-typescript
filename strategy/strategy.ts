aec1c2b74e89c4f79c225cea0af479aa69de2bc7
gztchan <gztchan@gmail.com>
2016-10-04 15:20:31 +0800

    upload strategy

---
+++ strategy/strategy.ts
@@ -0,0 +1,40 @@
+interface WorkoutStrategy {
+  fire(): void;
+}
+
+class Running implements WorkoutStrategy {
+  public fire(): void {
+    console.log('Running')
+  }
+}
+
+class Basketball implements WorkoutStrategy {
+  public fire(): void {
+    console.log('Basketball')
+  }
+}
+
+class Swimming implements WorkoutStrategy {
+  public fire(): void {
+    console.log('Swimming')
+  }
+}
+
+class Person {
+  public strategy: WorkoutStrategy;
+  public name: String;
+  constructor(name: string, strategy: WorkoutStrategy) {
+    this.name = name;
+    this.strategy = strategy
+  }
+
+  workout(): void {
+    console.log(`${ this.name } starts:`)
+    this.strategy.fire();
+  };
+}
+
+(function main() {
+  const amanda = new Person('Amanda', new Running())
+  amanda.workout()
+})()
