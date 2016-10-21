34a8aade2cdb759d1e50017d1552f1f887b3a9a6
gztchan <gztchan@gmail.com>
2016-10-21 14:21:24 +0800

    Template Method Design Pattern added

---
+++ template-method/template-method.ts
@@ -0,0 +1,42 @@
+class BaseClass {
+  public temlpateMethod(): void {
+    this.actionA();
+    this.actionB();
+  }
+
+  public actionA(): void {
+    throw new Error('should not be invoker by BaseClass');
+  };
+
+  public actionB(): void {
+    throw new Error('should not be invoker by BaseClass');
+  }
+}
+
+class ConcreteAClass extends BaseClass {
+  actionA(): void {
+    console.log('A take actionA')
+  }
+
+  actionB(): void {
+    console.log('A take actionB')
+  }
+}
+
+class ConcreteBClass extends BaseClass {
+  actionA(): void {
+    console.log('B take actionA')
+  }
+
+  actionB(): void {
+    console.log('B take actionB')
+  }
+}
+
+(function main() {
+  const a = new ConcreteAClass();
+  const b = new ConcreteBClass();
+
+  a.temlpateMethod();
+  b.temlpateMethod();
+})();
